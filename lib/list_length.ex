a236793982b61bbc0b9b0f25e5c952a94e4315e7
Victor Moraes de Aragao <vct.aragao@gmail.com>
2021-05-12 20:01:13 -0300

    Funçao recusiva para o calculo do tamanho de uma lista

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: check_length(list, 0)
+
+  defp check_length([], size), do: size
+
+  defp check_length([_ | tail], size) do
+    check_length(tail, size + 1)
   end
 end
